9c5e964c600b046f6af1ee69bc05fff7a9852bf0
SameGevDudios <magomed.chapaev1@gmail.com>
2025-08-06 19:01:06 +0300

    Create game initialization

--- Assets/Scripts/Bootstrap/Bootstrap.cs
+++ Assets/Scripts/Bootstrap/Bootstrap.cs
@@ -1,18 +1,41 @@
-using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Bootstrap : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [Header("Object pooling")]
+    [SerializeField] private List<Pool> _pools = new();
+
+    [Header("Waves")]
+    [SerializeField] private List<Wave> _waves = new();
+
+    [Header("Player")]
+    [SerializeField] private Vector3 _playerPosition;
+    [SerializeField] private int _playerHealth, _playerDamage;
+    [SerializeField] private float _applyDamageSpeed, _attackSpeed;
+
+    [Header("Enemies")]
+
+    [SerializeField] private int _enemyHealth, _enemyDamage;
+
+    private void Awake()
     {
-        
+        // Instantiate pool manager
+        IPoolManager poolManager = new PoolManager(_pools);
+        Battling player = poolManager.InstantiateFromPool("player", _playerPosition, Quaternion.identity)
+            .GetComponent<Battling>();
+        IWaveSpawner waveSpawner = 
+            new WaveSpawner(poolManager, _waves, _enemyHealth, _enemyDamage, _applyDamageSpeed, _attackSpeed);
+        IBattle battle = new Battle(waveSpawner, player);
+        player.Init(battle, _playerHealth, _playerDamage, _applyDamageSpeed, _attackSpeed);
+
+        // Start game
+        battle.Attack();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnDrawGizmosSelected()
     {
-        
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawWireSphere(_playerPosition, 0.1f);
     }
 }
