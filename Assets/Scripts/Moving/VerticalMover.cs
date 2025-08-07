b021bb425bef5b93229e51dea295837ec9f3fe1b
SameGevDudios <magomed.chapaev1@gmail.com>
2025-08-07 16:15:11 +0300

    Rename variable

--- Assets/Scripts/Moving/VerticalMover.cs
+++ Assets/Scripts/Moving/VerticalMover.cs
@@ -3,19 +3,19 @@ using UnityEngine;
 
 public class VerticalMover : IMover
 {
-    private Transform _camera;
+    private Transform _movable;
     private float _moveDistance, _moveDuration;
 
-    public VerticalMover(Transform camera, float moveDistance, float moveDuration)
+    public VerticalMover(Transform movable, float moveDistance, float moveDuration)
     {
-        _camera = camera;
+        _movable = movable;
         _moveDistance = moveDistance;
         _moveDuration = moveDuration;
     }
 
     public void Move()
     {
-        _camera.DOMoveY(_camera.position.y + _moveDistance, _moveDuration);
+        _movable.DOMoveY(_movable.position.y + _moveDistance, _moveDuration);
         _moveDistance = -_moveDistance;
     }
 }
