05b570a701d5d8048c9cbd64e6adbe37c7fafde8
SameGevDudios <magomed.chapaev1@gmail.com>
2025-08-07 17:42:34 +0300

    Add maze spawn for maze event

--- Assets/Scripts/MazeAnimation/MazeEvent.cs
+++ Assets/Scripts/MazeAnimation/MazeEvent.cs
@@ -1,10 +1,12 @@
 public class MazeEvent : IGameEvent
 {
+    private ISpawner _spawner;
     private VerticalMover _cameraMover, _mazeMover;
     private IEventCaller _eventCaller;
 
-    public MazeEvent(VerticalMover cameraMover, VerticalMover mazeMover, IEventCaller eventCaller)
+    public MazeEvent(ISpawner spawner, VerticalMover cameraMover, VerticalMover mazeMover, IEventCaller eventCaller)
     {
+        _spawner = spawner;
         _cameraMover = cameraMover;
         _mazeMover = mazeMover;
         _eventCaller = eventCaller;
@@ -12,6 +14,7 @@ public class MazeEvent : IGameEvent
 
     public void Play()
     {
+        _mazeMover.SetNewMovable(_spawner.Spawn().transform);
         _cameraMover.Move();
         _mazeMover.Move();
         // Delay
