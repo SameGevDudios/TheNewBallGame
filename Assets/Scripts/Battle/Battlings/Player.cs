c0b448c58928e447f95c0d3d357e650a057187a4
SameGevDudios <magomed.chapaev1@gmail.com>
2025-08-07 18:03:23 +0300

    Add camera access for player

--- Assets/Scripts/Battle/Battlings/Player.cs
+++ Assets/Scripts/Battle/Battlings/Player.cs
@@ -1,8 +1,16 @@
+using UnityEngine;
+
 public class Player : Battling
 {
+    [SerializeField] private Transform _camera;
     protected override void Death()
     {
         base.Death();
         _battle.PlayerKilled();
+    }
+
+    public Transform GetCamera()
+    {
+        return _camera;
     }
 }
